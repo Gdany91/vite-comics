5be567e8fcaa4614b747058c3e7b0827e10a3dce
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-12-16 15:56:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script >
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name:'app',
+   
+  components : {
+      AppHeader
+  }
+}
+
+</script> 
+
+<template>
+  <div>
+     <AppHeader />
+
+  </div>
+</template>
+ 
+<style lang="scss"  >
+
+  @use './styles/general.scss' as *;
+  @use './styles/partials/variables.scss' as *;
+
+    h1{
+      color: $primary;
+    }
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+    export default {
+        name: 'AppHeader',
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            
+            <a href="" class = logo>
+                <img src="./src/assets/dc-logo.png" alt="">
+            </a>
+
+            
+
+            <div class="menu">
+
+            </div>
+
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+ .container{
+    background-color: rebeccapurple;
+    min-height: 80px;
+    display: flex;
+   
+ }
+
+</style>
